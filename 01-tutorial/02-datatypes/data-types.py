e8b1ec3d1f9a9bf4658ee3b36e3b4a760d09745e
siraajul <shirajulislamparvez@gmail.com>
2025-08-03 22:35:05 +0600

    data types

---
+++ 01-tutorial/02-datatypes/data-types.py
@@ -0,0 +1,24 @@
+# data-types - provide value to variable
+
+#------------------------Numbers-------------------------------
+# Integers
+a = -32
+print(a)
+
+# Float
+b = 7.27
+print(b)
+
+# Complex
+v = 34j
+print(v)
+
+#-----------------------Strings--------------------------------
+# need to use "" / ''
+st = 'abcd 1234 @#$%'
+print(type(st))
+
+#----------------------Boolean---------------------------------
+f = True
+e = False
+print(type(f))
