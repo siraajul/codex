b41508146dafcc9b2702d70128ef4ba36ab3b7a0
siraajul <shirajulislamparvez@gmail.com>
2025-08-03 22:33:44 +0600

    comments

---
+++ 01-tutorial/01-comments-and-variable/01-comments.py
@@ -0,0 +1,11 @@
+# we have to use '#'for writing comments in python
+
+#doc string
+#we can achieve multiline comments in python using dc strings
+"""
+Hello
+This
+Is The
+Multiline
+Comments in Python
+"""
