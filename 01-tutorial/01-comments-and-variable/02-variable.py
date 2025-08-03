48b97014875bb4a663501a9bab8afb6afc6d560f
siraajul <shirajulislamparvez@gmail.com>
2025-08-03 22:33:53 +0600

    variables

---
+++ 01-tutorial/01-comments-and-variable/02-variable.py
@@ -0,0 +1,20 @@
+#in python variable are used as a storage to store things in python
+#can write anything in variable name
+
+sher = 'Siraj'
+print(sher)
+
+# can't use number at start
+# can't use spaces in variables
+# can't use special char in variables
+
+#naming convention
+
+#Camel case
+sheriyansSchool = 'Students'
+
+#Pascal case
+SheriyansSchool = 'Student'
+
+#Snake case
+sheriyans_school = 'Student'
